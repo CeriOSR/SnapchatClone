00861d4e55d739b61ac5bd02cfca29823b2a495a
CeriOSR <CeriOSrey@gmail.com>
2017-02-02 15:49:56 -0800

    Initial Commit

---
+++ CeriosSnaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CeriosSnaps
+//
+//  Created by Rey Cerio on 2017-02-02.
+//  Copyright © 2017 CeriOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
